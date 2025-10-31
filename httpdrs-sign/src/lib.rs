26383a0dc7a6ef2bd4c3182980612ff29f335777
史红光 <hgshi@baai.ac.cn>
2025-11-01 01:57:42 +0800

    init

--- httpdrs-sign/src/lib.rs
+++ httpdrs-sign/src/lib.rs
@@ -1,14 +1,18 @@
-pub struct DigitalSignatureAlgorithm{
+use crate::reader::ReaderRequest;
+
+mod reader;
+
+pub struct SignatureClient{
     client: reqwest::Client,
-    url: String,
+    reader_presign: String,
 }
 
 
-impl DigitalSignatureAlgorithm {
+impl SignatureClient {
     pub fn new() -> Self {
-        DigitalSignatureAlgorithm {
+        SignatureClient {
             client: reqwest::Client::new(),
-            url: "https://www.baidu.com".to_string(),
+            reader_presign: "http://127.0.0.1:30002/api/v1/provider/download/presign".to_string(),
         }
     }
 
@@ -20,10 +24,17 @@ impl DigitalSignatureAlgorithm {
         Ok(text)
     }
 
-    pub async fn download_sign(&self) -> Result<String, reqwest::Error> {
-        let response = self.client.get(self.url.clone()).send().await?;
-        let text = response.text().await?;
+    pub async fn reader_get(&self, sign_data: String) -> Result<String, reqwest::Error> {
+        let req = ReaderRequest::new("public", "".to_string());
+
+        let reader_presign = self.reader_presign.as_str();
+        let resp = self.client.post(reader_presign).json(&req).send().await?;
+        let text = resp.text().await?;
         Ok(text)
+    }
+
+    pub async fn writer_get(&self, sign_data: String) -> Result<String, reqwest::Error> {
+        Ok("".to_string())
     }
 }
 
@@ -33,8 +44,8 @@ mod tests {
 
     #[tokio::test]
     async fn test_ping() {
-        let s = DigitalSignatureAlgorithm::new();
-        let result = s.ping_get().await.unwrap();
+        let signature = SignatureClient::new();
+        let result = signature.ping_get().await.unwrap();
         println!("{}", result)
     }
 }
---
+++ httpdrs-sign/src/reader.rs
@@ -0,0 +1,32 @@
+use std::fmt::Display;
+use serde::{Deserialize, Serialize};
+
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct ReaderRequest<'a> {
+    pub network: &'a str,
+    pub download_sign: String,
+}
+
+impl ReaderRequest<'_> {
+    pub fn new(network: &'_ str, download_sign: String) -> ReaderRequest<'_> {
+        ReaderRequest {
+            network,
+            download_sign,
+        }
+    }
+}
+
+impl Display for ReaderRequest<'_> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}: {}", self.network, self.download_sign)
+    }
+}
+
+
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct ReaderResponse {
+    pub status: String,
+    pub message: String,
+}
