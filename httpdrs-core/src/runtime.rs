2f7b8861cadc7b6f62ac9517a2e3fba29019a876
史红光 <hgshi@baai.ac.cn>
2025-10-31 19:08:48 +0800

    init

--- httpdrs-core/src/runtime.rs
+++ httpdrs-core/src/runtime.rs
@@ -1,7 +1,19 @@
+use std::sync::{Arc, Mutex, LazyLock};
+
+
 use tokio::runtime;
 use futures::future::join_all;
-
 use crate::reader;
+
+#[allow(dead_code)]
+#[derive(Debug, Default)]
+pub struct EventReader{
+    pub total_size: i64,
+}
+
+static RUNTIME_STATS: LazyLock<Arc<Mutex<EventReader>>> =
+    LazyLock::new(|| Arc::new(Mutex::new(EventReader::default())));
+
 
 pub fn start_multi_thread() -> Result<(), Box<dyn std::error::Error>>{
 
@@ -13,13 +25,13 @@ pub fn start_multi_thread() -> Result<(), Box<dyn std::error::Error>>{
 
     tracing::info!("Runtime initialized: baai-flagdataset-rs");
 
-    let event_reader = rt.spawn(async {
+    let spawn_reader = rt.spawn(async {
         let mut csv_reader = reader::CSVMetaReader::new("/Users/hgshicc/test/flagdataset/AIM-500/meta".to_string());
         let _ = csv_reader.init().await;
         tracing::info!("CSVReader initialized: baai-flagdataset-rs: {}", csv_reader);
     });
 
-    let event_tasks = vec![event_reader];
+    let event_tasks = vec![spawn_reader];
 
     let _ = rt.block_on(join_all(event_tasks));
     rt.shutdown_background();
