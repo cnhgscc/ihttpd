944b83588712f490f7a33cc6e69e7f411dcac3f9
史红光 <hgshi@baai.ac.cn>
2025-10-31 16:24:40 +0800

    init

--- baai_flagdataset_rs/cmd/cmd_run.py
+++ baai_flagdataset_rs/cmd/cmd_run.py
@@ -1,4 +1,37 @@
-from ..baai_flagdataset_rs import run_flagdataset
+import argparse
 
-def run_with_cmdargs():
-    run_flagdataset()
+def with_cmdargs():
+
+    root_parser = argparse.ArgumentParser(add_help=False)
+
+    parser = argparse.ArgumentParser(prog='baai-flagdataset', description="baai-flagdataset 命令行工具: [bf]")
+    subparsers = parser.add_subparsers(dest='command')
+
+    init_parser = subparsers.add_parser('init', help='init', parents=[root_parser])
+    init_parser.set_defaults(func=init_with_cmdargs)
+
+    cmd_args = parser.parse_args()
+    if hasattr(cmd_args, 'func'):
+        try:
+            cmd_args.func(cmd_args)
+        except Exception: # noqa
+            pass
+        except KeyboardInterrupt:
+            print()
+            pass
+    else:
+        parser.print_help()
+
+
+def init_with_cmdargs(cmd_args):
+
+    try:
+        from ..baai_helper import baai_print
+
+
+        baai_print.print_figlet()
+
+        print("flagdataset 初始化完成")
+    except Exception as e:
+        print(e)
+
