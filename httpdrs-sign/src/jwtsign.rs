e5fdab7a11db53d384c5e60e52467d63ce4602e5
史红光 <hgshi@baai.ac.cn>
2025-11-01 16:19:02 +0800

    init

--- httpdrs-sign/src/jwtsign.rs
+++ httpdrs-sign/src/jwtsign.rs
@@ -7,13 +7,13 @@ use rmp_serde::from_slice;
 use serde::Deserialize;
 
 #[derive(Debug, Deserialize)]
-pub struct  MetaData{
+pub struct  HttpdMetaReader{
     pub proto: String,
     pub path: String,
     pub prefix: String,
 }
 
-impl MetaData {
+impl HttpdMetaReader {
     pub fn local_relative_path(&self) -> PathBuf {
         PathBuf::from(&self.prefix).join(&self.path)
     }
@@ -25,20 +25,58 @@ impl MetaData {
         PathBuf::from(base_dir).join(self.local_relative_path())
     }
 
+    pub fn check_local_file(&self, base_dir: &str) -> Option<u64> {
+        let path = self.local_absolute_path_str(base_dir);
+        check_file_meta(path)
+    }
+
+    pub fn local_part_path(&self, base_dir: &str, part_index: u32, temp_dir: &str) -> PathBuf {
+        // 获取本地绝对路径
+        let local_path = self.local_absolute_path_str(base_dir);
+
+        // 计算文件路径的MD5哈希
+        let absolute_path_str = local_path.canonicalize()
+            .unwrap_or(local_path.clone())
+            .to_string_lossy()
+            .to_string();
+        let file_hash = format!("{:x}", md5::compute(absolute_path_str.as_bytes()));
+
+        // 获取文件名
+        let file_name = local_path
+            .file_name()
+            .and_then(|name| name.to_str())
+            .unwrap_or("unknown");
+
+        // 生成分片文件名: {part_index}__{file_hash}__{file_name}.bin
+        let part_filename = format!("{}__{}__{}.bin", part_index, file_hash, file_name);
+        PathBuf::from(temp_dir).join(part_filename)
+    }
+
 }
 
-impl Display for MetaData {
+impl Display for HttpdMetaReader {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{:?}", self.local_relative_path())
     }
 }
 
-pub fn jwtsign(token: String) -> Result<MetaData, Box<dyn std::error::Error>> {
+pub fn jwtsign(token: String) -> Result<HttpdMetaReader, Box<dyn std::error::Error>> {
     let t = Token::<Header, Claims, _>::parse_unverified(&*token).unwrap();
     let claims =  t.claims().clone();
 
     let download_path = claims.private.get("download_path").unwrap().to_string().trim_matches('"').to_string();
     let base64_decoded = BASE64_STANDARD.decode(download_path)?;
-    let meta_data: MetaData = from_slice(&base64_decoded)?;
-    Ok(meta_data)
+    let meta_reader: HttpdMetaReader = from_slice(&base64_decoded)?;
+    Ok(meta_reader)
 }
+
+pub fn check_file_meta(file_path: PathBuf) -> Option<u64> {
+    if file_path.is_file() {
+        std::fs::metadata(&file_path)
+            .ok()
+            .map(|meta| meta.len())
+    } else {
+        None
+    }
+}
+
