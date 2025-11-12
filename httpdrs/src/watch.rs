fd5a1dcc9517c9a8e497ad6124ae54bbaec6294f
史红光 <hgshi@baai.ac.cn>
2025-11-12 18:39:05 +0800

    feat: speed

--- httpdrs/src/watch.rs
+++ httpdrs/src/watch.rs
@@ -1,13 +1,20 @@
-use crate::stats::RUNTIME;
 use tokio_util::sync::CancellationToken;
+use tokio::time::Instant;
 use indicatif::ProgressBar;
+
 use httpdrs_core::pbar;
 
+use crate::stats::RUNTIME;
+
+
 pub(crate) async fn init(pb:ProgressBar, token_bandwidth: CancellationToken){
+    let start = Instant::now();
+
     pb.set_message(pbar::format(0, 0, 0, 0.0));
+
     loop {
         tokio::select! {
-            _ = tokio::time::sleep(tokio::time::Duration::from_millis(300)) => {
+            _ = tokio::time::sleep(tokio::time::Duration::from_millis(1000)) => {
                 let (require_bytes, require_count) = {
                     let runtime = RUNTIME.lock().unwrap();
                     (runtime.require_bytes, runtime.require_count)
@@ -28,9 +35,17 @@ pub(crate) async fn init(pb:ProgressBar, token_bandwidth: CancellationToken){
                     _ => (download_bytes + completed_bytes) as f64 / require_bytes as f64,
                 };
 
+                let use_ms = start.elapsed().as_millis();
+                let period_download_bytes =  RUNTIME.lock().unwrap().download_bytes;
+                let period_speed = match use_ms {
+                    0 => 0,
+                    _ => period_download_bytes  / use_ms as u64  * 1000,
+                };
+
+                // TODO: 进度条显示
                 pb.set_length(RUNTIME.lock().unwrap().require_count);
                 pb.set_position(completed_count + download_count);
-                pb.set_message(pbar::format(require_bytes, require_count, RUNTIME.lock().unwrap().download_speed, download_percent));
+                pb.set_message(pbar::format(require_bytes, require_count, period_speed, download_percent));
             }
             _ = token_bandwidth.cancelled() => {
                 break;
