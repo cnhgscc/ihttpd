b424b45520164ecb7091400137739506a984cd59
史红光 <hgshi@baai.ac.cn>
2025-11-15 14:50:07 +0800

    init pbar

--- httpdrs-pbar/src/lib.rs
+++ httpdrs-pbar/src/lib.rs
@@ -14,20 +14,22 @@ pub fn create() -> ProgressBar {
 
 pub fn format(
     require_bytes: u64,
-    require_count: u64,
     download_speed: u64,
     download_precent: f64,
+    download_bytes: u64,
 ) -> String {
     let require_bytes_human = HumanBytes(require_bytes);
-    let require_count_human = HumanCount(require_count);
 
     let download_speed_human = HumanBytes(download_speed);
+    let download_bytes_human = HumanBytes(download_bytes);
 
+    // 1. 下载大小的进度
+    // 2. 下载速度
     format!(
-        "|  {:.2}%   | {}/s | required({}/{})",
+        "| {}/{} |{:.2}%|{}/s",
+        download_bytes_human,
+        require_bytes_human
         download_precent * 100.0,
         download_speed_human,
-        require_count_human,
-        require_bytes_human
     )
 }
--- httpdrs/src/watch.rs
+++ httpdrs/src/watch.rs
@@ -9,7 +9,7 @@ use crate::stats::RUNTIME;
 pub(crate) async fn init(pb: ProgressBar, token_bandwidth: CancellationToken) {
     let start = Instant::now();
 
-    pb.set_message(pbar::format(0, 0, 0, 0.0));
+    pb.set_message(pbar::format(0, 0, 0.0, 0));
 
     let mut last_count: u64 = 0;
     let mut last_bytes: u64 = 0;
@@ -22,28 +22,38 @@ pub(crate) async fn init(pb: ProgressBar, token_bandwidth: CancellationToken) {
                 let start_bytes = last_bytes;
 
 
+                // 统计信息
                 let (require_bytes, require_count) = {
                     let runtime = RUNTIME.lock().unwrap();
                     (runtime.require_bytes, runtime.require_count)
                 };
 
+                // 成功信息
                 let (completed_bytes, completed_count) = {
                     let runtime = RUNTIME.lock().unwrap();
                     (runtime.completed_bytes, runtime.completed_count)
                 };
 
+                // 失败信息
+                let (uncompleted_bytes, uncompleted_count) = {
+                    let runtime = RUNTIME.lock().unwrap();
+                    (runtime.uncompleted_bytes, runtime.uncompleted_count)
+                };
+
+                // 断点续传信息
                 let (download_bytes, download_count) = {
                     let runtime = RUNTIME.lock().unwrap();
                     (runtime.download_bytes, runtime.download_count)
                 };
 
-                last_count = completed_count + download_count;
-                last_bytes = completed_bytes + download_bytes;
-
+                // 下载百分比, 断点续传+成功+失败 / 总量
                 let download_percent = match require_bytes {
                     0 => 0.0,
-                    _ => (download_bytes + completed_bytes) as f64 / require_bytes as f64,
+                    _ => (download_bytes + completed_bytes + uncompleted_bytes) as f64 / require_bytes as f64,
                 };
+
+                // 计算带宽(成功)
+                last_bytes = completed_bytes;
 
                 let use_ms = start.elapsed().as_millis();
                 let period_bytes = last_bytes-start_bytes;
@@ -56,11 +66,14 @@ pub(crate) async fn init(pb: ProgressBar, token_bandwidth: CancellationToken) {
                     last_speed = period_speed;
                 }
 
+                // 所有处理过的文件数量 成功+失败+断点续传
+                last_count = completed_count + uncompleted_count + download_count;
+
                 tracing::info!("download_watch, last_speed: {}/s", HumanBytes(last_speed));
-                // TODO: 进度条显示
-                pb.set_length(RUNTIME.lock().unwrap().require_count);
+                pb.set_length(require_count);
                 pb.set_position(last_count);
-                pb.set_message(pbar::format(require_bytes, require_count, period_speed, download_percent));
+
+                pb.set_message(pbar::format(require_bytes, period_speed, download_percent, download_bytes + completed_bytes + uncompleted_bytes));
             }
             _ = token_bandwidth.cancelled() => {
                 break;
