617537dbbd47480709e0d5b7418cf6a6ed76d40f
史红光 <hgshi@baai.ac.cn>
2025-11-17 07:02:02 +0800

    init log

--- httpdrs-logger/src/lib.rs
+++ httpdrs-logger/src/lib.rs
@@ -1,6 +1,6 @@
 use tracing_appender::rolling;
 pub fn try_logger_init(log_path: &str) {
-    let appender = rolling::daily(log_path, "app.log");
+    let appender = rolling::never(log_path, "httpdrs.log");
 
     let subscriber = tracing_subscriber::fmt()
         .with_line_number(true)
@@ -8,7 +8,11 @@ pub fn try_logger_init(log_path: &str) {
         .with_thread_ids(true)
         .with_ansi(false)
         .with_writer(appender) // 显示线程ID
-        .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
+        .with_env_filter(
+            tracing_subscriber::EnvFilter::builder()
+                .with_default_directive(tracing::Level::WARN.into())
+                .from_env_lossy()  // 优先使用环境变量，失败则回退到默认值
+        )
         .finish();
 
     tracing::subscriber::set_global_default(subscriber).expect("Failed to set subscriber");
